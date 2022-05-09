209bb5416bc9aef7f9636b36670658acd9c41153
ph8nt0m <phant0m@kakao.com>
2022-05-10 08:32:59 +0900

    feat: Number Func

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,13 +7,66 @@
 
 import UIKit
 
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case Unknown
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var seconeOperand = ""
+    var result = ""
+    var currentOperation: Operation = .Unknown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+        
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.seconeOperand = ""
+        self.result = ""
+        self.currentOperation = .Unknown
+        self.numberOutputLabel.text = "0"
+    }
+    
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        if self.displayNumber.count < 8, !self.displayNumber.contains(".") {
+            self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tapDevideButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapMultiplyButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapSubtractButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+    }
 }
 
